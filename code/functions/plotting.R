27b9138ab9f4dca5fcdee9431638ceb182b8d836
Eleanor Jackson <eleanor.elizabeth.j@gmail.com>
2026-07-20 17:58:58 +0100

    Create plotting.R

---
+++ code/functions/plotting.R
@@ -0,0 +1,76 @@
+# Project-wide plotting conventions ----------------------------------------
+
+sbe_colours <- c(
+	"1-species" = "#009E73",
+	"4-species" = "#E69F00FF",
+	"16-species" = "#56B4E9FF",
+	"16-species-cut" = "#F0E442"
+)
+
+sbe_labels <- c(
+	"1-species" = "Single species",
+	"4-species" = "4 Species mix",
+	"16-species" = "16 Species mix",
+	"16-species-cut" = "16 Species mix\nliana cutting",
+	"compl" = "Complementarity effect",
+	"selec" = "Selection effect",
+	"net" = "Net biodiversity effect"
+)
+
+sbe_relabel <- function(x) {
+	x <- as.character(x)
+	matched <- match(x, names(sbe_labels))
+	has_label <- !is.na(matched)
+
+	x[has_label] <- unname(sbe_labels[matched[has_label]])
+	x
+}
+
+sbe_labeller <- ggplot2::as_labeller(sbe_relabel)
+
+theme_sbe <- function(
+	base_size = 10,
+	base_family = "",
+	ink = "#202020",
+	paper = "#FFFFFF",
+	accent = "#D55E00FF"
+) {
+	ggplot2::theme_bw(
+		base_size = base_size,
+		base_family = base_family,
+		ink = ink,
+		paper = paper,
+		accent = accent
+	) +
+		ggplot2::theme(
+			palette.colour.discrete = "Okabe-Ito",
+			palette.fill.discrete = "Okabe-Ito",
+			palette.colour.continuous = scales::pal_viridis(),
+			palette.fill.continuous = scales::pal_viridis(),
+			legend.position = "bottom"
+		)
+}
+
+scale_colour_sbe <- function(
+	...,
+	values = sbe_colours,
+	labels = sbe_relabel
+) {
+	ggplot2::scale_colour_manual(
+		...,
+		values = values,
+		labels = labels
+	)
+}
+
+scale_fill_sbe <- function(
+	...,
+	values = sbe_colours,
+	labels = sbe_relabel
+) {
+	ggplot2::scale_fill_manual(
+		...,
+		values = values,
+		labels = labels
+	)
+}
